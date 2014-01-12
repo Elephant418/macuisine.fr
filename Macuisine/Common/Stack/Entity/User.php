062f40aefe5e911e4845cf55a6e2d20505fb29a2
Simon Boyer <simon.boyer@openwide.fr>
2014-01-12 17:41:09 +0100

    Remove file settings on User entities

--- Macuisine/Common/Stack/Entity/User.php
+++ Macuisine/Common/Stack/Entity/User.php
@@ -2,14 +2,6 @@
 namespace Macuisine\Common\Stack\Entity;
 
 class User extends User\__Parent {
-
-    public function fetchByLogin($login)
-    {
-        $userList = $this->fetchByField('login', $login, 1);
-        if (count($userList)) {
-            return $userList[0];
-        }
-        return null;
-    }
+    
 }
 ?>
